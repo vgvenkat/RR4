dbba05f3be71c6b80b120d3078e51a54ddb034cd
vgvenkat <vgkrish619@gmail.com>
2017-03-15 16:22:58 -0700

    adding links and Routes 🔗

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
-console.log(BrowserRouter)
-const App = () => <h1>Hello World</h1>
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import './App.css'
+const Home = (props) => {
+    console.log(props);
+    return <h1>Home</h1>
+}
+
+const Links = () => (
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to={{pathname:"/about"}}>About</Link>
+        <Link replace to="/children">Children</Link>
+        
+    </nav>
+)
+const App = () => (
+    <Router>
+        <div>
+            <Links/>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" render={()=> <h1>About</h1>} />
+            <Route path="/children" children={({match})=>  match && <h1>Children</h1>} />
+
+        </div>
+    </Router>
+)
 
 export default App
